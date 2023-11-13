50fb24607d7f9a35f622e3ee067e7674408cfd0c
12Yura21 <115339891+12Yura21@users.noreply.github.com>
2023-11-13 21:01:33 +0200

    Add files via upload

---
+++ pw3/script.js
@@ -0,0 +1,72 @@
+const addButton = document.querySelector('input[type="submit"]');
+
+addButton.addEventListener("click", function(event) {
+  const input = document.querySelector('input[type="text"]').value;
+  const table = document.querySelector("table");
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+
+  li.textContent = input;
+  if(li.textContent.trim() === ""){
+    alert("Incorrect item!")
+  }else{
+  const button = document.createElement("button");
+  const edit = document.createElement("button");
+  button.addEventListener("click", function(event) {
+    if(confirm("Are you sure you want to delete this item?")){
+      button.parentNode.remove();
+    }
+  });
+
+  edit.addEventListener("click", function(){
+    let text =  button.previousSibling.nodeValue
+    const changeItem = document.createElement("input");
+    const saveButton = document.createElement("button");
+    const cancelButton = document.createElement("button");
+
+    changeItem.value = text;
+    button.previousSibling.nodeValue = "";
+
+    cancelButton.addEventListener("click",function (){
+      saveButton.remove();
+      cancelButton.remove();
+      changeItem.remove()
+      button.classList.toggle('hide');
+      edit.classList.toggle('hide');
+      button.previousSibling.nodeValue = text;
+    });
+    saveButton.addEventListener("click", function (){
+      if(changeItem.value.trim() === ''){
+        alert('Incorrect item!')
+      }else {
+        button.previousSibling.nodeValue = changeItem.value;
+        saveButton.remove();
+        cancelButton.remove();
+        changeItem.remove()
+        button.classList.toggle('hide');
+        edit.classList.toggle('hide');
+      }
+    });
+
+    saveButton.textContent = "Save"
+    cancelButton.textContent = "Cancel"
+
+    li.appendChild(changeItem);
+    li.appendChild(saveButton);
+    li.appendChild(cancelButton);
+    changeItem.focus()
+    button.classList.toggle('hide');
+    edit.classList.toggle('hide');
+   });
+
+  edit.textContent = "Edit";
+  button.textContent = "Delete";
+  li.appendChild(button);
+  li.appendChild(edit);
+  ul.appendChild(li);
+  table.appendChild(ul);
+
+  document.querySelector("input[name='item']").value = "";
+  document.querySelector('input[type="text"]').focus();
+  }
+});
